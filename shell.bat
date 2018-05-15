96db47368260b42d9fa74fb1ef3294707472532d
fushall <fushall@foxmail.com>
2018-05-15 17:39:11 +0800

    shell.bat能用了

--- shell.bat
+++ shell.bat
@@ -1,9 +1,9 @@
 call D:\PythonVenv\flask\Scripts\activate
-set FLASK_APP=%cd%\myblog\manage.py
+set FLASK_APP=myblog\manage.py
 set FLASK_ENV=development
 set FLASK_DEBUG=1
 
-call flask shell
-call cmd
+
+call cmd 
 
 pause
